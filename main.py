ea7a4e3b5cd930d28d58f09095422cde2b5bdf25
varun029 <varun.kaushik@jktech.com>
2025-03-14 10:07:48 +0530

    add gitignore, make modular functions, edit main.py

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import pandas as pd
 import numpy as np
+from utils.read import read_file
+from utils.missing import miss_val_column
+from utils.missing import miss_val_percentage
 
 #loading the dataset
-df=pd.read_csv('/Users/vandana/Desktop/steam-200k.csv')
+df=read_file('/Users/vandana/Desktop/steam-200k.csv')
+
+column_names=['user_id','game-title','behavior','value','unknown']
+df.columns=column_names
 
 #basic info about dataset
 print('dataset info: ')
-print(df.info())
+print(df.describe())
 
 #print first 5 rows
 print(df.head())
+print('\n')
 
 #check for missing values
-print('\nMissing Value count:')
-print(df.isnull().sum())
+missing_values=miss_val_column(df)
 
 # Percentage of missing values
-print("\nPercentage of Missing Values:")
-print((df.isnull().sum() / len(df)) * 100)
+percent_missing=miss_val_percentage(df,missing_values)
 
 #count duplicate rows
 duplicate_rows=df.duplicated().sum()
@@ -26,3 +31,4 @@ print(f'total duplicate rows: {duplicate_rows}')
 if duplicate_rows>0:
     print('Duplicate rows are:\n')
     print(df[df.duplicated()])
+
---
+++ utils/read.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def read_file(path):
+    df=pd.read_csv(path)
+    #print(df.dtypes)
+    #print(df.head())
+    print('\n')
+    return df
