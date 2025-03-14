d1c244738ee041482a490aa9a45ec7578c5625ab
varun029 <varun.kaushik@jktech.com>
2025-03-14 15:20:44 +0530

    add drop.py and drop the unnecessary column in the dataset

--- main.py
+++ main.py
@@ -3,32 +3,39 @@ import numpy as np
 from utils.read import read_file
 from utils.missing import miss_val_column
 from utils.missing import miss_val_percentage
+from utils.drop import drop_column
 
 #loading the dataset
 df=read_file('/Users/vandana/Desktop/steam-200k.csv')
 
+#add column names
 column_names=['user_id','game-title','behavior','value','unknown']
 df.columns=column_names
 
+#drop unnecessary columns
+df_dropped=drop_column(df,'unknown')
+
 #basic info about dataset
 print('dataset info: ')
-print(df.describe())
+print(df_dropped.describe())
 
 #print first 5 rows
-print(df.head())
+print(df_dropped.head())
 print('\n')
 
 #check for missing values
-missing_values=miss_val_column(df)
+missing_values=miss_val_column(df_dropped)
 
 # Percentage of missing values
-percent_missing=miss_val_percentage(df,missing_values)
+percent_missing=miss_val_percentage(df_dropped,missing_values)
 
 #count duplicate rows
-duplicate_rows=df.duplicated().sum()
+duplicate_rows=df_dropped.duplicated().sum()
 print(f'total duplicate rows: {duplicate_rows}')
 
 if duplicate_rows>0:
     print('Duplicate rows are:\n')
-    print(df[df.duplicated()])
+    print(df_dropped[df_dropped.duplicated()])
+
+
 
